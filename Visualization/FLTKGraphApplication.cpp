f0bcf0de0fee243cad24ab9d2389ad1890512515
MichaelKolesnikov <michaelkolesnikov056@mail.ru>
2024-07-26 02:17:01 +0300

    buttons was added

--- Visualization/FLTKGraphApplication.cpp
+++ Visualization/FLTKGraphApplication.cpp
@@ -16,7 +16,8 @@ using namespace std;
 class GraphApp : public Fl_Double_Window {
 private:
     Fl_Input *vertex1_input, *vertex2_input, *weight_input;
-    Fl_Button *add_vertex_button, *add_edge_button, *remove_vertex_button, *remove_edge_button, *find_cycle_button;
+    Fl_Button *add_vertex_button, *add_edge_button, *remove_vertex_button, *remove_edge_button,
+        *find_cycle_button, *top_sort_button, *min_span_tree_button, *comps_button, *min_path_button, *find_clique_button;
     Fl_Check_Button *directed_check, *weighted_check, *multiple_edges_check;
     Fl_Box* graph_display;
     Fl_PNG_Image* graph_image;
@@ -25,7 +26,8 @@ private:
     bool weighted = false;
     bool with_multiple_edges = false;
     Graphs::DictOfVertices<string> vertices_000, vertices_001, vertices_010, vertices_011, vertices_100, vertices_101, vertices_110, vertices_111;
-    Graphs::UndirectedGraph<string> graph_000, graph_001, graph_010, graph_011, graph_100, graph_101, graph_110, graph_111;
+    Graphs::UndirectedGraph<string> graph_000, graph_001, graph_010, graph_011;
+    Graphs::DirectedGraph<string> graph_100, graph_101, graph_110, graph_111;
 
     Graphs::IGraph<string>* get_current_graph() {
         Graphs::IGraph<string>* graph = nullptr;
@@ -121,7 +123,14 @@ public:
         add_edge_button = new Fl_Button(230, 40, 120, 25, "Add Edge");
         remove_vertex_button = new Fl_Button(230, 70, 120, 25, "Remove Vertex");
         remove_edge_button = new Fl_Button(230, 100, 120, 25, "Remove Edge");
-        find_cycle_button = new Fl_Button(230, 130, 120, 25, "Find Cycle");
+
+        find_cycle_button = new Fl_Button(360, 10, 120, 25, "Find Cycle");
+        find_clique_button = new Fl_Button(360, 40, 120, 25, "Find clique");
+        min_span_tree_button = new Fl_Button(360, 70, 120, 25, "Span tree");
+        comps_button = new Fl_Button(360, 100, 120, 25, "Comps");
+        min_path_button = new Fl_Button(360, 100, 120, 25, "Min path");
+        top_sort_button = new Fl_Button(230, 130, 120, 23, "Top sort");
+        top_sort_button->deactivate();
 
         directed_check = new Fl_Check_Button(10, 130, 120, 25, "Directed");
         weighted_check = new Fl_Check_Button(10, 160, 120, 25, "Weighted");
@@ -139,7 +148,14 @@ public:
         add_edge_button->callback(static_add_edge_cb, this);
         remove_vertex_button->callback(static_remove_vertex_cb, this);
         remove_edge_button->callback(static_remove_edge_cb, this);
+
         find_cycle_button->callback(static_find_cycle_cb, this);
+        find_clique_button->callback(static_find_clique_cb, this);
+        min_span_tree_button->callback(static_min_span_tree_cb, this);
+        comps_button->callback(static_comps_cb, this);
+        min_path_button->callback(static_min_path_cb, this);
+        top_sort_button->callback(static_top_sort_cb, this);
+
         directed_check->callback(static_toggle_directed_cb, this);
         weighted_check->callback(static_toggle_weighted_cb, this);
         multiple_edges_check->callback(static_toggle_multiple_edges_cb, this);
@@ -152,7 +168,14 @@ public:
     static void static_add_edge_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->add_edge_cb(); }
     static void static_remove_vertex_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->remove_vertex_cb(); }
     static void static_remove_edge_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->remove_edge_cb(); }
+
     static void static_find_cycle_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->find_cycle_cb(); }
+    static void static_find_clique_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->find_clique_cb(); }
+    static void static_min_span_tree_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->min_span_tree_cb(); }
+    static void static_comps_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->comps_cb(); }
+    static void static_min_path_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->min_path_cb(); }
+    static void static_top_sort_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->top_sort_cb(); }
+
     static void static_toggle_directed_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->toggle_directed_cb(); }
     static void static_toggle_weighted_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->toggle_weighted_cb(); }
     static void static_toggle_multiple_edges_cb(Fl_Widget* w, void* v) { ((GraphApp*)v)->toggle_multiple_edges_cb(); }
@@ -167,7 +190,6 @@ public:
             update_graph_display();
         }
     }
-
     void add_edge_cb() {
         auto current_vertices = get_current_vertices();
         auto current_graph = get_current_graph();
@@ -177,13 +199,19 @@ public:
         if (weighted) {
             weight = std::stoi(weight_input->value());
         }
-        if (current_vertices->find(v1) != current_vertices->end() &&
-            current_vertices->find(v2) != current_vertices->end()) {
-            current_graph->add_edge({(*current_vertices)[v1], (*current_vertices)[v2], weighted, weight});
+        if (current_vertices->find(v1) == current_vertices->end()) {
+            (*current_vertices)[v1] = Graphs::Vertex<std::string>(v1);
+            current_graph->add_vertex((*current_vertices)[v1]);
             update_graph_display();
         }
+        if (current_vertices->find(v2) == current_vertices->end()) {
+            (*current_vertices)[v2] = Graphs::Vertex<std::string>(v2);
+            current_graph->add_vertex((*current_vertices)[v2]);
+            update_graph_display();
+        }
+        current_graph->add_edge({(*current_vertices)[v1], (*current_vertices)[v2], weighted, weight});
+        update_graph_display();
     }
-
     void remove_vertex_cb() {
         auto current_vertices = get_current_vertices();
         auto current_graph = get_current_graph();
@@ -194,7 +222,6 @@ public:
             update_graph_display();
         }
     }
-
     void remove_edge_cb() {
         auto current_vertices = get_current_vertices();
         auto current_graph = get_current_graph();
@@ -224,11 +251,148 @@ public:
         graph_display->image(graph_image);
         graph_display->redraw();
     }
+    void find_clique_cb() {
+        auto g = get_current_graph();
+        auto und = dynamic_cast<Graphs::UndirectedGraph<string>*>(g);
+        auto clique = und->clique_search();
+        std::string filename = "output_graph.dot";
+        std::string output_image = "output_image.png";
+
+        Graphs::VertexColors<string> c;
+        for (auto& v : clique) {
+            c[v] = "red";
+        }
+
+        Graphs::create_dot_file(*und, filename, "black", static_cast<Graphs::EdgeColors<string>*>(nullptr), &c);
+        Graphs::create_picture(filename, output_image);
+        delete graph_image;
+        graph_image = new Fl_PNG_Image(output_image.c_str());
+        graph_display->image(graph_image);
+        graph_display->redraw();
+    }
+    void min_span_tree_cb() {
+        auto g = get_current_graph();
+        auto& vertices = *get_current_vertices();
+        auto graph = dynamic_cast<Graphs::UndirectedGraph<string>*>(g);
+        auto span = graph->finding_minimum_spanning_tree();
+        auto set_ = span.get_edges();
+        auto colors = Graphs::EdgeColors<string>();
+        for (auto& it : set_) {
+            colors[it] = "green";
+        }
+        std::string filename = "output_graph.dot";
+        std::string output_image = "output_image.png";
+
+        Graphs::create_dot_file(*graph, filename, "black", &colors);
+        Graphs::create_picture(filename, output_image);
+        delete graph_image;
+        graph_image = new Fl_PNG_Image(output_image.c_str());
+        graph_display->image(graph_image);
+        graph_display->redraw();
+    }
+    void comps_cb() {
+        auto g = get_current_graph();
+        auto comps = g->get_connected_components();
+        std::string filename = "output_graph.dot";
+        std::string output_image = "output_image.png";
+
+        std::ofstream dotFile(filename);
+        string edge_s = " -- ";
+        if (g->is_directed()) {
+            edge_s[2] = '>';
+            dotFile << "di";
+        }
+
+        dotFile << "graph G {" << std::endl;
+
+        int num_cluster = 0;
+        for (auto& vv : comps) {
+            dotFile << "subgraph cluster_" << ++num_cluster << " {" << endl;
+            for (auto& v : vv) {
+                dotFile << v.get_object() << ";\n";
+            }
+            dotFile << "}" << endl;
+        }
+
+        for (Graphs::Edge<string> edge : g->get_edges()) {
+            dotFile << edge.vertex1.get_object() << edge_s << edge.vertex2.get_object();
+            dotFile << " [";
+            if (g->is_weighted()) {
+                dotFile << "label=" << edge.get_weight();
+            }
+            dotFile << "]" << std::endl;
+            dotFile << ";" << std::endl;
+        }
+        dotFile << "}" << std::endl;
+        dotFile.close();
+
+        Graphs::create_picture(filename, output_image);
+        delete graph_image;
+        graph_image = new Fl_PNG_Image(output_image.c_str());
+        graph_display->image(graph_image);
+        graph_display->redraw();
+    }
+    void min_path_cb() {
+        string s1 = vertex1_input->value();
+        string s2 = vertex2_input->value();
+        auto graph = get_current_graph();
+        auto vertices = get_current_vertices();
+        if (vertices->find(s1) == vertices->end() || vertices->find(s2) == vertices->end()) {
+            return;
+        }
+        auto path = graph->shortest_way(vertices->at(s1), vertices->at(s2));
+
+        std::string filename = "output_graph.dot";
+        std::string output_image = "output_image.png";
+
+        auto colors = Graphs::EdgeColors<string>();
+        for (auto& it : path) {
+            colors[it] = "yellow";
+        }
+        Graphs::create_dot_file(*graph, filename, "black", &colors);
+        Graphs::create_picture(filename, output_image);
+        delete graph_image;
+        graph_image = new Fl_PNG_Image(output_image.c_str());
+        graph_display->image(graph_image);
+        graph_display->redraw();
+    }
+    void top_sort_cb() {
+        auto graph = get_current_graph();
+        auto dg = dynamic_cast<Graphs::DirectedGraph<string>*>(graph);
+        auto ts = dg->topological_sorting();
+        if (ts.size() == 0) {
+            // this->top_sort_result->Text = "Impossible";
+            return;
+        }
+        std::stringstream ss;
+        ss << "{rank=same; ";
+        for (auto v : ts) {
+            ss << v.get_object() << "; ";
+        }
+        ss << "}";
+        std::string additional_command = ss.str();
+        // this->top_sort_result->Text = gcnew String(additional_command.c_str());
+        std::string filename = "output_graph.dot";
+        std::string output_image = "output_image.png";
+
+        Graphs::create_dot_file(*graph, filename, "black", static_cast<Graphs::EdgeColors<string>*>(nullptr), static_cast<Graphs::VertexColors<string>*>(nullptr), "", additional_command);
+        Graphs::create_picture(filename, output_image);
+
+        delete graph_image;
+        graph_image = new Fl_PNG_Image(output_image.c_str());
+        graph_display->image(graph_image);
+        graph_display->redraw();
+    }
 
     void toggle_directed_cb() {
         auto current_vertices = get_current_vertices();
         auto current_graph = get_current_graph();
         directed = directed_check->value();
+        if (directed) {
+            top_sort_button->activate();
+        } else {
+            top_sort_button->deactivate();
+        }
         current_graph = get_current_graph();
         update_graph_display();
     }
